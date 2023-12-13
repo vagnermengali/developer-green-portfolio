482b60543532fb506d28482da2da2b4fee1503a6
vagnermengali <vagmengali2015@gmail.com>
2023-12-13 17:53:06 -0300

    feat: add new qa

--- src/components/Templates/About/About.tsx
+++ src/components/Templates/About/About.tsx
@@ -55,12 +55,6 @@ const About = () => {
                 </div>
                 <div className="text-xs uppercase tracking-wide tracking-[1px] leading-[1.4] max-w-[100px]">Anos de experiência</div>
               </div>
-              {/*               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={650} duration={5} /> +
-                </div>
-                <div className="text-xs uppercase tracking-wide tracking-[1px] leading-[1.4] max-w-[100px]">Clientes Satisfeitos</div>
-              </div> */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={75} duration={10} /> +
@@ -73,12 +67,6 @@ const About = () => {
                 </div>
                 <div className="text-xs uppercase tracking-wide tracking-[1px] leading-[1.4] max-w-[100px]">Avaliações positivas</div>
               </div>
-              {/*               <div className="relative flex-1">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={8} duration={5} /> +
-                </div>
-                <div className="text-xs uppercase tracking-wide tracking-[1px] leading-[1.4] max-w-[100px]">Ganhando prêmios</div>
-              </div> */}
               <div className="relative flex-1">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={17} duration={5} /> +
--- src/data/indications.tsx
+++ src/data/indications.tsx
@@ -3,7 +3,7 @@ export const testimonialSlider = [
     id: "testimonial1",
     image: "/image/adam.jpg",
     name: "Adam Neves",
-    position: "Desenvolvedora Front End",
+    position: "Desenvolvedor Front End",
     message:
       "Vagner é um desenvolvedor fullstack excepcional com quem tive o privilégio de estudar na Kenzie Academy. Durante nosso tempo juntos, sempre se destacou consistentemente pela sua proatividade, criatividade e excelência técnica. Sua habilidade em transformar ideias inovadoras em códigos robustos e funcionais é notável. Demonstrou uma dedicação extraordinária aos projetos, sempre superando expectativas e contribuindo de maneira significativa para o sucesso da equipe.",
   },
@@ -11,7 +11,7 @@ export const testimonialSlider = [
     id: "testimonial2",
     image: "/image/matheus.jpg",
     name: "Matheus Sudré",
-    position: "Desenvolvedora Front End",
+    position: "Desenvolvedor Front End",
     message:
       "É com prazer que recomendo o Vagner como um talentoso Desenvolvedor Front-end, com bastante experiencia em React e possui excelente experiencia na plataforma VTEX. Sua habilidade técnica excepcional, capacidade de resolver problemas de forma criativa e comunicação clara tornam-no um recurso valioso para qualquer equipe, se destaca pela adaptabilidade e contribuição proativa, destacando-se como um profissional comprometido e colaborativo.",
   },
@@ -19,7 +19,7 @@ export const testimonialSlider = [
     id: "testimonial3",
     image: "/image/kelven.jpg",
     name: "Kelven Souza",
-    position: "Desenvolvedora Front End",
+    position: "Desenvolvedor Front End",
     message:
       "Excelente como pessoa e como profissional. Desenvolvedor ágil, proativo e extremamente dedicado. Recomendo 100%!",
   },
@@ -27,7 +27,7 @@ export const testimonialSlider = [
     id: "testimonial4",
     image: "/image/amon.jpg",
     name: "Amon Fanticelli",
-    position: "Desenvolvedora Full Stack",
+    position: "Desenvolvedor Full Stack",
     message:
       "Tive a oportunidade de colaborar e trabalhar com o Mengali, um desenvolvedor excepcional. Apresentou diversas habilidades e dedicação nas tarefas que realizou.",
   },
@@ -35,7 +35,7 @@ export const testimonialSlider = [
     id: "testimonial5",
     image: "/image/marcus.jpg",
     name: "Marcus Vinicius",
-    position: "Desenvolvedora Full Stack",
+    position: "Desenvolvedor Full Stack",
     message:
       "Mengali é um profissional exemplar no campo do Front-End, destacando-se não apenas pelo seu amplo conhecimento, mas também pela sua maestria na utilização de ferramentas como VTEX IO e CMS. Demonstrando uma sintonia constante com as inovações da área, ele se destaca por manter-se sempre atualizado, adotando as versões mais recentes dos frameworks e dedicando esforços incansáveis para refatorar o código. Sua busca pela perfeição é notável, evidenciando-se não apenas na preocupação em manter o código atualizado, mas também na incansável busca por melhorias, resultando em uma estrutura modularizada que facilita a reutilização e manutenção. Mengali personifica o comprometimento com a excelência e a habilidade de integrar conhecimentos específicos, como VTEX IO e CMS, em seu trabalho diário.",
   },
@@ -43,7 +43,7 @@ export const testimonialSlider = [
     id: "testimonial6",
     image: "/image/vinicius.jpeg",
     name: "Vinícius Moreira Henrique",
-    position: "Desenvolvedora Full Stack",
+    position: "Desenvolvedor Full Stack",
     message:
       "Escrevo para expressar minha mais alta recomendação para Vagner Mengali, com quem tive o prazer de trabalhar na Kenzie Academy Brasil. Durante nosso tempo juntos, Vagner demonstrou uma compreensão profunda e habilidade prática nas tecnologias que usamos em nosso curso de Desenvolvedor Full Stack. Ele se destacou não apenas na compreensão dos conceitos, mas também na aplicação prática deles em nosso projeto conjunto. Vagner é proficiente em várias tecnologias modernas, incluindo HTML, CSS, JavaScript, React no front-end e SQL, Python e Node.js no back-end. Sua capacidade de aplicar essas habilidades de maneira eficaz contribuiu significativamente para o sucesso de nosso projeto. Vagner é um profissional dedicado, sempre disposto a colocar o tempo e o esforço necessários para entregar um trabalho de alta qualidade. Ele é um excelente comunicador, capaz de explicar conceitos complexos de maneira clara e concisa. Estou confiante de que Vagner será uma adição valiosa para qualquer equipe de desenvolvimento e recomendo-o sem reservas.",
   },
@@ -51,7 +51,7 @@ export const testimonialSlider = [
     id: "testimonial7",
     image: "/image/gabriel.jpeg",
     name: "Gabriel Fray",
-    position: "Desenvolvedora Front End",
+    position: "Desenvolvedor Front End",
     message:
       "Vagner Mengali é um colega excepcional, destacando-se não apenas por sua competência técnica, mas também por sua notável habilidade no âmbito do companheirismo e criatividade em design (UX/UI). Tive a satisfação de colaborar em diversos projetos com ele, onde sua presença foi um diferencial marcante. Sua dedicação ao trabalho em equipe é evidente, e a considero um verdadeiro ponto forte. Vagner não apenas contribui com suas habilidades técnicas, mas também promove um ambiente colaborativo e positivo, sem dúvidas um parceiro valioso em qualquer projeto. Além disso, sua expertise em design, especialmente em UX/UI, é notável. Vagner possui o talento único de criar sites visualmente atraentes e funcionais. Seus trabalhos são verdadeiras obras de arte, refletindo sua criatividade e atenção aos detalhes. Essa combinação de habilidades técnicas e visão estética torna suas contribuições excepcionais.",
   },
@@ -59,14 +59,14 @@ export const testimonialSlider = [
     id: "testimonial8",
     image: "/image/yuran.jpg",
     name: "Yuran Vita",
-    position: "Desenvolvedora Full Stack",
+    position: "Desenvolvedor Full Stack",
     message:
       "Dedicado e sempre disposto a ajudar.",
   }, {
     id: "testimonial9",
     image: "/image/joao.jpg",
     name: "João Porfirio",
-    position: "Desenvolvedora Front End",
+    position: "Desenvolvedor Front End",
     message:
       "Trabalhar com Vagner foi uma experiência incrível! Sua expertise técnica em vtex e seu comprometimento com a qualidade foram essenciais para o sucesso do projeto. Além disso, sua atitude proativa e habilidades de resolução de problemas fazem dele um desenvolvedor excepcional. Recomendo Vagner sem hesitação!",
   },
@@ -81,7 +81,7 @@ export const testimonialSlider = [
     id: "testimonial11",
     image: "/image/enilson.jpg",
     name: "Enilson Silva",
-    position: "Desenvolvedora Front End",
+    position: "Desenvolvedor Front End",
     message:
       "Um exelente Dev!",
   },
