2e1259be28e972ac96e27b3662feee0124c8eaee
vagnermengali <vagmengali2015@gmail.com>
2023-12-13 11:07:37 -0300

    feat: add new qa

--- src/components/Other/WorkSlider/WorkSlider..tsx
+++ src/components/Other/WorkSlider/WorkSlider..tsx
@@ -29,7 +29,7 @@ const WorkSlider = () => {
                     <Link href={image.href || "#"} target="_blank" passHref>
                     <div className="flex items-center justify-center relative overflow-hidden group">
                       <Image alt={image.href} src={image.path} width={500} height={300} />
-                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#ffc857] to-[#FFF] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#08d665] to-[#FFF] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       <div  className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
                         <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
                           <div className="delay-100">VER</div>
