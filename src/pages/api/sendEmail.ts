90fd1991e703b087e563b5d8dfbb02337dfbe2f7
vagnermengali <vagmengali2015@gmail.com>
2023-12-18 16:13:20 -0300

    feat: add new qa

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,39 +1,49 @@
+import nodemailer, { Transporter, SendMailOptions } from 'nodemailer';
 import { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 
-dotenv.config();
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function SendEmail(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
+    return res.status(405).json({ error: 'Método não permitido' });
   }
 
-  const { from, to, subject, text } = req.body;
+  const { email, subject, html, name } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || '',
-    port: parseInt(process.env.SMTP_PORT || '465', 10),
-    secure: true,
+  // Verifica se as variáveis de ambiente estão definidas
+  if (!process.env.SMTP_USER || !process.env.SMTP_PASS) {
+    console.error('Erro: Variáveis de ambiente SMTP não estão definidas');
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+
+  // Cria uma instância do transportador SMTP
+  const transporter: Transporter = nodemailer.createTransport({
+    service: 'Gmail',
     auth: {
-      user: process.env.SMTP_USER || '',
-      pass: process.env.SMTP_PASS || '',
+      user: process.env.SMTP_USER, // Seu endereço de e-mail do Gmail
+      pass: process.env.SMTP_PASS, // Sua senha do Gmail
     },
   });
 
+  // Configuração do e-mail
+  const mailOptions: SendMailOptions = {
+    from: process.env.SMTP_USER,
+    to: email,
+    subject: subject,
+    html: `${name} - ${html}`,
+  };
+
+  // Envia o e-mail
   try {
-    const info = await transporter.sendMail({
-      from,
-      to,
-      subject,
-      text,
-    });
-
-    console.log('E-mail enviado:', info.messageId);
-
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email enviado:', info.messageId);
     res.status(200).json({ success: true, messageId: info.messageId });
-  } catch (error) {
-    console.error('Erro ao enviar e-mail:', (error as Error).message);
-    res.status(500).json({ error: 'Erro interno ao enviar e-mail' });
+  } catch (error: any) {
+    console.error('Erro ao enviar e-mail:', error);
+    res.status(500).json({ error: error });
   }
 }
+
+export default SendEmail;
