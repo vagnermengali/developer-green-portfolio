d12aef5722ee2972aca1e67c23ac20fdb8a1bc08
vagnermengali <vagmengali2015@gmail.com>
2023-12-19 19:14:35 -0300

    feat: add mobile

--- src/components/Other/TestimonialSlider/TestimonialSlider.tsx
+++ src/components/Other/TestimonialSlider/TestimonialSlider.tsx
@@ -21,7 +21,7 @@ const TestimonialSlider = () => {
       {testimonialSlider.map((person, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
+            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full xl:px-16 px-4">
               <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
                 <div className="flex flex-col justify-center text-center">
                   <div className="mb-2 mx-auto">
@@ -32,10 +32,10 @@ const TestimonialSlider = () => {
                 </div>
               </div>
               <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
-                <div className="mb-4">
+                <div className="xl:flex hidden mb-4">
                   <RiDoubleQuotesL className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
                 </div>
-                <div className="xl:text-lg text-center md:text-left">{person.message}</div>
+                <div className="xl:text-lg text-center md:text-left xl:mt-0 mt-3">{person.message}</div>
               </div>
             </div>
           </SwiperSlide>
