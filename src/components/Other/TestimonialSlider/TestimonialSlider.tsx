f4bc9bc45704acde72062cd139f27ee7ba4412cb
vagnermengali <vagmengali2015@gmail.com>
2023-12-19 19:33:57 -0300

    feat: add mobile

--- src/components/Other/TestimonialSlider/TestimonialSlider.tsx
+++ src/components/Other/TestimonialSlider/TestimonialSlider.tsx
@@ -31,7 +31,7 @@ const TestimonialSlider = () => {
                   <div className="text-[12px] uppercase font-extralight tracking-widest">{person.position}</div>
                 </div>
               </div>
-              <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+              <div className="flex-1 flex flex-col xl:justify-center justify-start before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
                 <div className="xl:flex hidden mb-4">
                   <RiDoubleQuotesL className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
                 </div>
