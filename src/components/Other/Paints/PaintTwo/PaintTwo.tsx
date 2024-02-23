b5978ec75b5ae0a259a7144303e375b3725e5fdf
vagnermengali <vagmengali2015@gmail.com>
2024-02-23 17:14:44 -0300

    feat: add new qa

--- src/components/Other/Paints/PaintTwo/PaintTwo.tsx
+++ src/components/Other/Paints/PaintTwo/PaintTwo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const PaintTwo = () => {
   
-  return <div className="absolute -left-20 -top-20 rotate-[240deg] mix-blend-color-dodge z-10 w-[200px] xl:w=[400px] opacity-50">
-    <Image src='/green-background-3.webp' width={400} height={400} alt=""/>
+  return <div className="absolute -left-20 -top-20 rotate-[240deg] mix-blend-color-dodge z-10 w-[200px] xl:w=[440px] opacity-50">
+    <Image src='/green-background-3.webp' width={440} height={440} alt=""/>
   </div>;
 };
 
