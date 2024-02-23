62acd7138aea070ea971ae83856e3b4a2d9b4ec6
vagnermengali <vagmengali2015@gmail.com>
2024-02-23 17:08:06 -0300

    feat: add new qa

--- src/components/Other/ProjectsBtn/ProjectsBtn.tsx
+++ src/components/Other/ProjectsBtn/ProjectsBtn.tsx
@@ -5,17 +5,17 @@ import { RiArrowRightLine } from "react-icons/ri";
 const ProjectsBtn = () => {
   
   return (
-    <div className="mx-auto xl:m-0 flex justify-center items-center">
+    <div className="mx-auto xl:m-0 flex justify-center items-center cursor-pointer">
       <Link
         href={"/work"}
-        className="relative w-[135px] h-[135px] flex justify-center items-center bg-circle bg-cover bg-center bg-no-repeat group"
+        className="relative w-[135px] h-[135px] flex justify-center items-center bg-circle bg-cover bg-center bg-no-repeat group cursor-pointer"
         aria-label={"work"}
       >
         <Image
           src={"/rounded-text.webp"}
           fill
           alt="rounded-text"
-          className="animate-spin-slow w-full h-full max-w-[101px] max-h-[108px] inset-auto"
+          className="animate-spin-slow w-full h-full max-w-[111px] max-h-[118px] m-auto cursor-pointer"
         />
         <RiArrowRightLine className="absolute text-4xl group-hover:translate-x-2 transation-all duration-300" />
       </Link>
