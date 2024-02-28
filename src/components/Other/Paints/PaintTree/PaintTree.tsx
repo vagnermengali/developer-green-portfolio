593437b31b7cb10b7a49054fe3b9c3bc946379c2
vagnermengali <vagmengali2015@gmail.com>
2024-02-28 16:07:34 -0300

    feat: add new qa

--- src/components/Other/Paints/PaintTree/PaintTree.tsx
+++ src/components/Other/Paints/PaintTree/PaintTree.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Circles = () => {
   
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
+    <div className="hidden xl:flex w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
       <Image
         src={"/green-background-2.webp"}
         width={260}
