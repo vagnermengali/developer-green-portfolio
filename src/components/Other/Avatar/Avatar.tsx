0142c9aca7a90fffeaea00927d58961a647b8226
vagnermengali <vagmengali2015@gmail.com>
2024-03-04 23:02:45 -0300

    feat: add new about

--- src/components/Other/Avatar/Avatar.tsx
+++ src/components/Other/Avatar/Avatar.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
-
 import { AvatarInterface } from "@/interfaces/AvatarInterface";
 
 const Avatar = ({ opacity }: AvatarInterface) => {
-
   return (
     <div className="flex max-w-none">
       <Image
         src={"/avatar.webp"}
-        width={1000}
-        height={1000}
+        width={1080}
+        height={1080}
         alt=""
         loading="eager"
         className={`translate-z-0 w-full h-full ${opacity ? 'xl:opacity-50 opacity-20' : 'xl:opacity-70 opacity-20'}`}
