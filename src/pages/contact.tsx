959737e061901b9a38898b29ec4b9c1b0fc3d92a
vagnermengali <vagmengali2015@gmail.com>
2024-03-04 23:14:26 -0300

    feat: add new about

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,5 @@
-import dynamic from 'next/dynamic';
-
-const Seo = dynamic(() => import('@/components/Other/Seo'));
-const Contact = dynamic(() => import('@/components/Templates/Contact/Contact'));
+import Seo from "@/components/Other/Seo";
+import Contact from "@/components/Templates/Contact/Contact";
 
 const ContactPage = () => {
   return (
