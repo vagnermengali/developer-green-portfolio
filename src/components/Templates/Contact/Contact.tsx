c6373f09a8456d42acaf9939a446bcb31e20f4da
vagnermengali <vagmengali2015@gmail.com>
2024-03-08 14:59:26 -0300

    feat: add new qa

--- src/components/Templates/Contact/Contact.tsx
+++ src/components/Templates/Contact/Contact.tsx
@@ -85,7 +85,7 @@ const Contact = () => {
               animate="show"
               exit="hidden"
               onSubmit={handleSubmit(onSubmit)}
-              className="flex-1 flex flex-col gap-6 w-full mx-auto text-xs"
+              className="flex-1 flex flex-col gap-6 w-full mx-auto"
             >
               <div className="flex gap-x-6 w-full">
                 <Controller
