bd509f15f5d11af5e21971d45cee26512efa2595
vagnermengali <vagmengali2015@gmail.com>
2024-03-30 11:25:06 -0300

    feat: add new qa

--- src/components/Other/Seo/index.tsx
+++ src/components/Other/Seo/index.tsx
@@ -23,11 +23,11 @@ const Seo = ({ title, description }: SeoInterface) => {
             <meta property="og:type" content="website" key="ogtype" />
             <meta property="og:url" content="https://vagnermengali.com/" />
             <meta property="og:image:alt" content={description} />
-            <meta property="og:image" content="/portfolio-blue.webp" />
+            <meta property="og:image" content="/portfolio-green.webp" />
             <meta name="twitter:title" content="Vagner Mengali" />
             <meta name="twitter:site" content="@vagnermengali" />
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:image" content="/portfolio-blue.webp" />
+            <meta name="twitter:image" content="/portfolio-green.webp" />
             <meta name="twitter:description" content={description} />
             <meta property="al:ios:app_name" content="Vagner Mengali" />
             <meta property="al:ios:url" content="https://vagnermengali.com/" />
