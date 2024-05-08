58cd3549e731bbe567be68a051aae380c344b211
Vagner Mengali <vagmengali2015@gmail.com>
2024-05-08 16:31:26 -0300

    Update services.tsx

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -5,7 +5,7 @@ const ServicesPage = () => {
   return (
     <>
       <Seo
-        title={'Serviços'}
+        title={'Serviços • Vagner Mengali • Portfólio'}
         description="Descubra uma gama abrangente de serviços de desenvolvimento de software personalizados para atender às necessidades de empresas e empreendedores. De desenvolvimento web e mobile a consultoria em TI, estou pronto para transformar suas ideias em realidade digital."
       />
       <Services />
