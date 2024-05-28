f9acab80fae14aa85e13e7e3e01a432663202bf8
Vagner Mengali <vagmengali2015@gmail.com>
2024-05-28 08:20:13 -0300

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,7 +5,7 @@ const ContactPage = () => {
   return (
     <>
       <Seo
-        title={'Contato • Vagner Mengali • Portfólio'}
+        title="Contato • Vagner Mengali • Portfólio"
         description="Pronto para iniciar uma conversa sobre seu próximo projeto? Entre em contato comigo aqui. Estou ansioso para discutir suas ideias e como posso ajudá-lo a alcançar seus objetivos de desenvolvimento de software."
       />
       <Contact />
